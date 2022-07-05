700b0cd3936ccd926c255ecab764a002bb7acc73
sleepwallking <danislamov5@gmail.com>
2022-07-06 01:08:30 +0500

    Added static

--- first/cars/views.py
+++ first/cars/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+from .models import *
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти", "Зарегистрироваться"]
 
 
 def index(request):
-    return render(request, 'cars/index.html', {'menu': menu, 'title': 'Главная страница'})
+    posts = Cars.objects.all()
+    return render(request, 'cars/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
 
 
 def about(request):
